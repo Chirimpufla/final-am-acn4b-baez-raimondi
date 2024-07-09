90d55af881d0bb2fa224cf243cff9956d7536699
Guerzonianzu <cjbaez2012@gmail.com>
2024-07-09 16:06:43 -0300

    Registro Funcionando

--- app/src/main/java/com/example/final_am_acn4b_baez_raimondi/RegisterActivity.java
+++ app/src/main/java/com/example/final_am_acn4b_baez_raimondi/RegisterActivity.java
@@ -1,24 +1,124 @@
 package com.example.final_am_acn4b_baez_raimondi;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+    private ImageView avatar;
+    private EditText nombre, apellido, telefono, mail;
+    private Button registro;
+    private ProgressDialog carga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        avatar = findViewById(R.id.register_avatar);
+        avatar.setImageResource(R.drawable.avatar);
+
+        nombre = findViewById(R.id.register_name);
+
+        apellido = findViewById(R.id.register_surname);
+
+        mail = findViewById(R.id.register_mail);
+
+        telefono = findViewById(R.id.register_phone);
+
+        registro = findViewById(R.id.register_button);
+        registro.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        registrar();
+                    }
+                }
+        );
+    }
+
+    public void registrar(){
+
+        final String NAME = nombre.getText().toString().trim();
+        final String SURNAME = apellido.getText().toString().trim();
+        final String EMAIL = mail.getText().toString().trim();
+        final String PHONE = telefono.getText().toString().trim();
+        final String PASS = "123456";
+
+        if(NAME.isEmpty()){
+            nombre.setError("Este campo no puede estar vacio.");
+            nombre.requestFocus();
+        } else if (SURNAME.isEmpty()) {
+            apellido.setError("Este campo no puede estar vacio.");
+            apellido.requestFocus();
+        } else if (EMAIL.isEmpty()) {
+            mail.setError("Este campo no puede estar vacio.");
+            mail.requestFocus();
+        } else if (PHONE.isEmpty()){
+            telefono.setError("Este campo no puede estar vacio.");
+            telefono.requestFocus();
+        } else {
+            carga = new ProgressDialog(this);
+            carga.setTitle("Registrando usuario");
+            carga.setMessage("Por favor espere...");
+            carga.show();
+
+            mAuth.createUserWithEmailAndPassword(EMAIL, PASS)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                mAuth.signInWithEmailAndPassword(NAME, PASS)
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                Log.d("TAG", "Inicio de sesion exitoso.");
+                                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                                if(user != null){
+                                                    Toast.makeText(getBaseContext(), "Su contraseña es 123456. Puede cambiarla tocando la foto en la pagina principal.", Toast.LENGTH_LONG).show();
+                                                    homeRedirect(user);
+                                                }
+                                            }
+                                        });
+                            } else {
+                                carga.dismiss();
+                                Log.d(TAG, "Error al crear usuario: " + task.getException());
+                                Toast.makeText(getBaseContext(), "Error al crear el usuario. Por favor intente mas tarde.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void homeRedirect(FirebaseUser u){
+
+        String name = u.getDisplayName();
+        String mail = u.getEmail();
+
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("name", name);
+        i.putExtra("mail", mail);
+        startActivity(i);
+
     }
 }
